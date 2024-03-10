e9b7336aab11a06aaf3a73c09b2eae3c37cc2092
vinpap2008S <mirzoevsergej881@gmail.com>
2024-03-10 20:39:59 +0300

    C# Задание 1 Создайте приложение калькулятор для перевода числа из одной системы исчисления в другую. Пользователь с по мощью меню выбирает направление перевода. Например, из десятичной в двоичную. После выбора направления, пользователь вводит число в исходной системе исчисления. Приложение должно перевести число в требуемую систе му. Предусмотреть случай выхода за границы диапазона, определяемого типом int, неправильный ввод. Задание 2 Пользователь вводит словами цифру от 0 до 9. Прило жение должно перевести слово в цифру. Например, если пользователь ввёл five, приложение должно вывести на экран 5. Задание 3 Создайте класс «Заграничный паспорт». Вам необходимо хранить информацию о номере паспорта, ФИО владельца, дате выдачи и т.д. Предусмотреть механизмы для инициа лизации полей класса. Если значение для инициализации неверное, генерируйте исключение. Задание 4 Пользователь вводит в строку с клавиатуры логическое выражение. Например, 3>2 или 7<3. Программа должна посчитать результат введенного выражения и дать резуль тат true или false. В строке могут быть только целые числа и операторы: <, >, <=, >=, ==, !=. Для обработки ошибок ввода используйте механизм исключений реализуй

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,57 +1,119 @@
 ﻿using System;
 
+class Passport
+{
+    public string PassportNumber { get; set; }
+    public string FullName { get; set; }
+    public DateTime DateOfIssue { get; set; }
+
+    public Passport(string passportNumber, string fullName, DateTime dateOfIssue)
+    {
+        if (string.IsNullOrWhiteSpace(passportNumber) || string.IsNullOrWhiteSpace(fullName) || dateOfIssue == DateTime.MinValue)
+        {
+            throw new ArgumentException("Неверные данные для инициализации паспорта.");
+        }
+
+        PassportNumber = passportNumber;
+        FullName = fullName;
+        DateOfIssue = dateOfIssue;
+    }
+}
+
 class Program
 {
     static void Main()
     {
-        int[] array = { 1, 2, 3, 4, 5, 5, 6, 7, 8, 9 };
-        int evenCount = array.Count(x => x % 2 == 0);
-        int oddCount = array.Count(x => x % 2 != 0);
-        int uniqueCount = array.Distinct().Count();
-        Console.WriteLine($"Чётные: {evenCount}, Нечётные: {oddCount}, Уникальные: {uniqueCount}");
-
-    //Задание 2
-        int[] array = { 3, 5, 7, 2, 4, 6, 8 };
-            int userInput = int.Parse(Console.ReadLine());
-            int count = array.Count(x => x < userInput);
-            Console.WriteLine($"Количество значений меньше {userInput}: {count}");
-
-    // Задание 3
-        int[] array = { 7, 6, 5, 3, 4, 7, 6, 5, 8, 7, 6, 5 };
-            int[] userInput = { 7, 6, 5 };
-            int sequenceCount = Enumerable.Range(0, array.Length - userInput.Length + 1).Count(i => array.Skip(i).Take(userInput.Length).SequenceEqual(userInput));
-            Console.WriteLine($"Количество повторений последовательности: {sequenceCount}");
-
-    // Задание 4
-        int[] array1 = { 1, 2, 3, 4 };
-            int[] array2 = { 3, 4, 5, 6 };
-            var commonElements = array1.Intersect(array2).ToArray();
-            Console.WriteLine($"Общие элементы без повторений: {string.Join(", ", commonElements)}");
-
-    // Задание 5=
-        int[,] array = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
-            int min = array.Cast<int>().Min();
-            int max = array.Cast<int>().Max();
-            Console.WriteLine($"Минимальное значение: {min}, Максимальное значение: {max}");
-
-    // Задание 6
-    string sentence = Console.ReadLine();
-            int wordCount = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
-            Console.WriteLine($"Количество слов в предложении: {wordCount}");
-
-    // Задание 7
-        string sentence = Console.ReadLine();
-            string reversedSentence = string.Join(" ", sentence.Split(' ').Select(word => new string(word.Reverse().ToArray())));
-            Console.WriteLine($"После переворота: {reversedSentence}");
-    // Задание 8
-        string sentence = Console.ReadLine();
-            int vowelCount = sentence.Count(c => "aeiouAEIOU".Contains(c));
-            Console.WriteLine($"Количество гласных букв: {vowelCount}");
-
-    // Задание 9
-        string originalString = "Why she had to go. I don't know, she wouldn't say";
-            string substringToSearch = "she";
-            int occurrences = originalString.Split(new string[] { substringToSearch }, StringSplitOptions.None).Length - 1;
-            Console.WriteLine($"Результат поиска: {occurrences}");
+        try
+        {
+            Passport passport = new Passport("123456789", "Иванов Иван Иванович", new DateTime(2021, 12, 15));
+            Console.WriteLine("Паспорт успешно создан.");
         }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine("Ошибка при создании паспорта: " + ex.Message);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Выберите направление перевода:");
+        Console.WriteLine("1. Из десятичной в двоичную");
+        Console.WriteLine("2. Из двоичной в десятичную");
+
+        int choice;
+        while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 2)
+        {
+            Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+        }
+
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Введите число в десятичной системе:");
+                if (int.TryParse(Console.ReadLine(), out int decimalNumber))
+                {
+                    string binaryNumber = Convert.ToString(decimalNumber, 2);
+                    Console.WriteLine($"Число {decimalNumber} в двоичной системе: {binaryNumber}");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Некорректный ввод числа.");
+                }
+                break;
+            case 2:
+                Console.WriteLine("Введите число в двоичной системе:");
+                string binaryInput = Console.ReadLine();
+                if (int.TryParse(binaryInput, System.Globalization.NumberStyles.AllowBinarySpecifier, null, out int result))
+                {
+                    Console.WriteLine($"Число {binaryInput} в десятичной системе: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Некорректный ввод числа в двоичной системе.");
+                }
+                break;
+        }
+        Console.WriteLine("Введите слово от zero до nine:");
+        string inputWord = Console.ReadLine().ToLower();
+
+        switch (inputWord)
+        {
+            case "zero":
+                Console.WriteLine("0");
+                break;
+            case "one":
+                Console.WriteLine("1");
+                break;
+            case "two":
+                Console.WriteLine("2");
+                break;
+            case "three":
+                Console.WriteLine("3");
+                break;
+            case "four":
+                Console.WriteLine("4");
+                break;
+            case "five":
+                Console.WriteLine("5");
+                break;
+            case "six":
+                Console.WriteLine("6");
+                break;
+            case "seven":
+                Console.WriteLine("7");
+                break;
+            case "eight":
+                Console.WriteLine("8");
+                break;
+            case "nine":
+                Console.WriteLine("9");
+                break;
+            default:
+                Console.WriteLine("Некорректное слово. Введите слово от zero до nine.");
+                break;
+        }
+    }
 }
