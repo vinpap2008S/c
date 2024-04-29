000bed22ac551c3312d309275fb6c13d56b7889a
vinpap2008S <mirzoevsergej881@gmail.com>
2024-04-29 12:57:18 +0300

    дз

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,65 +1,79 @@
 ﻿using System;
-using System.IO;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        string filePath = "input.txt";
-        int totalCharsCount = 0;
-
-        using (StreamReader reader = new StreamReader(filePath))
+        var clients = new[]
         {
-            while (!reader.EndOfStream)
-            {
-                int charCode = reader.Read();
-                if (charCode != -1)
-                {
-                    char character = (char)charCode;
-                    Console.WriteLine($"{character}: {charCode}");
-                    totalCharsCount++;
-                }
-            }
+            new { Month = 1, Year = 2022, ClientCode = 1, Duration = 5 },
+            new { Month = 2, Year = 2022, ClientCode = 2, Duration = 7 },
+            new { Month = 3, Year = 2022, ClientCode = 3, Duration = 9 }
+        };
+
+        var maxDurationClient = clients.OrderByDescending(c => c.Duration).ThenByDescending(c => c.Year).ThenByDescending(c => c.Month).First();
+        Console.WriteLine($"Максимальная продолжительность занятий: {maxDurationClient.Duration}, Год: {maxDurationClient.Year}, Месяц: {maxDurationClient.Month}");
+
+        var applicants = new[]
+        {
+            new { Year = 2022, SchoolNumber = 1, LastName = "Иванов" },
+            new { Year = 2022, SchoolNumber = 2, LastName = "Петров" },
+            new { Year = 2023, SchoolNumber = 1, LastName = "Сидоров" }
+        };
+
+        var groupedApplicants = applicants.GroupBy(a => a.Year)
+                                          .Select(group => new { Year = group.Key, Count = group.Count() })
+                                          .OrderByDescending(g => g.Count)
+                                          .ThenBy(g => g.Year);
+
+        foreach (var group in groupedApplicants)
+        {
+            Console.WriteLine($"{group.Count} абитуриентов поступили в {group.Year} году");
         }
 
-        Console.WriteLine($"Общее количество символов: {totalCharsCount}");
 
-        
-        int wordCount = 0;
-
-        using (StreamReader reader = new StreamReader(filePath))
+        var applicants = new[]
         {
-            string text = reader.ReadToEnd();
-            string[] words = text.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-            wordCount = words.Length;
+            new { LastName = "Иванов", SchoolNumber = 1, Year = 2022 },
+            new { LastName = "Петров", SchoolNumber = 2, Year = 2022 },
+            new { LastName = "Сидоров", SchoolNumber = 1, Year = 2023 }
+        };
+
+        var maxApplicantsBySchool = applicants.GroupBy(a => a.SchoolNumber)
+                                              .Select(group => new { SchoolNumber = group.Key, Count = group.Count() })
+                                              .OrderByDescending(g => g.Count)
+                                              .First();
+
+        var maxApplicantsFromSchool = applicants.Where(a => a.SchoolNumber == maxApplicantsBySchool.SchoolNumber)
+                                                .OrderBy(a => a.SchoolNumber)
+                                                .ThenBy(a => a.LastName);
+
+        foreach (var applicant in maxApplicantsFromSchool)
+        {
+            Console.WriteLine($"{applicant.SchoolNumber} - {applicant.LastName}");
         }
 
-        Console.WriteLine($"Общее количество слов в файле: {wordCount}");
-
-        int lineCount = 0;
-
-        using (StreamReader reader = new StreamReader(filePath))
+        var students = new[]
         {
-            while (reader.ReadLine() != null)
-            {
-                lineCount++;
-            }
+            new { FirstName = "Иван", LastName = "Иванов", Age = 20, AverageScore = 4.5 },
+            new { FirstName = "Петр", LastName = "Петров", Age = 21, AverageScore = 4.8 },
+            new { FirstName = "Сидор", LastName = "Сидоров", Age = 19, AverageScore = 4.2 }
+        };
+
+        var topStudent = students.OrderByDescending(s => s.AverageScore).First();
+        Console.WriteLine($"Студент с самым высоким средним баллом: {topStudent.FirstName} {topStudent.LastName}, Возраст: {topStudent.Age}, Средний балл: {topStudent.AverageScore}");
+
+
+        var numbers = new[] { 10, 20, 30, 40, 50 };
+        double sum = 0;
+
+        foreach (var number in numbers)
+        {
+            sum += number;
         }
 
-        Console.WriteLine($"Общее количество строк в файле: {lineCount}");
-
-        int wordCount = 0;
-
-        using (StreamReader reader = new StreamReader(filePath))
-        {
-            string line;
-            while ((line = reader.ReadLine()) != null)
-            {
-                string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                wordCount += words.Length;
-            }
-        }
-
-        Console.WriteLine($"Общее количество слов в файле: {wordCount}");
+        double average = sum / numbers.Length;
+        Console.WriteLine($"Среднее арифметическое чисел: {average}");
     }
 }
