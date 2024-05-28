c6198d80b16de1a747fef1e9c20848b8c3bf6443
vinpap2008S <mirzoevsergej881@gmail.com>
2024-05-28 14:24:42 +0300

    дз

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,50 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text.Json;
-using ClassLib;
 
-namespace SerializDeserializExample
+class Program
 {
-    class Program
+    static string InvertCase(string input)
     {
-        static void Main()
-        {
-            // Создаем список объектов PC
-            List<PC> pcs = new List<PC>
+        int i = 0;
+        char[] result = new char[input.Length];
+        foreach (char c in input) {
+            if (char.IsLower(c))
             {
-                new PC("Dell", "12345"),
-                new PC("HP", "67890"),
-                new PC("Lenovo", "54321"),
-                new PC("Acer", "09876"),
-                new PC("Asus", "13579")
-            };
-
-            // Путь к файлу сериализации
-            string filePath = @"C:\path\to\file.json";
-
-            // Сериализация списка в формат JSON и сохранение в файл
-            string json = JsonSerializer.Serialize(pcs);
-            File.WriteAllText(filePath, json);
-
-            Console.WriteLine("Сериализация выполнена успешно");
-
-            // Десериализация объектов из файла и вывод на экран
-            if (File.Exists(filePath))
-            {
-                string jsonFromFile = File.ReadAllText(filePath);
-                List<PC> deserializedPcs = JsonSerializer.Deserialize<List<PC>>(jsonFromFile);
-
-                Console.WriteLine("Десериализация выполнена успешно. Результат:");
-                foreach (PC pc in deserializedPcs)
-                {
-                    Console.WriteLine(pc);
-                }
+                result[i] = char.ToUpper(c);
             }
             else
             {
-                Console.WriteLine("Файл не найден");
+                if (char.IsUpper(c))
+                {
+                    result[i] = char.ToLower(c);
+                }
+                else
+                {
+                    result[i] = c;
+                }
             }
+            i++;
         }
+        return new string(result);
+    }
+
+    static int[] CountPosSumNeg(float[] result)
+    {
+        if (result.Length == 0)
+            return new int[0];
+        int count = 0;
+        float sum = 0;
+        foreach(float f in result)
+        {
+            if (f > 0)
+                count++;
+            if (f < 0)
+                sum += f;
+        }
+        
+        return new int[] { count, ((int)sum) };
+
+    }
+
+    static void CountPosSumNegWrite(int[] result)
+    {
+        if (result.Length == 0)
+            Console.WriteLine("[]");
+        else
+            Console.WriteLine("[" + result[0] + ", " + result[1] + "]");
+
+
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(InvertCase("Happy bursdfsdD"));
+        Console.WriteLine(InvertCase("мама"));
+        Console.WriteLine(InvertCase("ПРорывО"));
+
+        CountPosSumNegWrite(CountPosSumNeg([]));
+        CountPosSumNegWrite(CountPosSumNeg([1, 2, 5, -3, -5]));
+
     }
 }
