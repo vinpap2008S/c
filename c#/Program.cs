0b3862e01c01e6aa741833ce88d0f6bde61d5717
vinpap2008S <mirzoevsergej881@gmail.com>
2024-06-21 11:34:15 +0300

    Патерн

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,68 +1,90 @@
 ﻿using System;
 
 class Program
+
 {
-    static string InvertCase(string input)
-    {
-        int i = 0;
-        char[] result = new char[input.Length];
-        foreach (char c in input) {
-            if (char.IsLower(c))
-            {
-                result[i] = char.ToUpper(c);
-            }
-            else
-            {
-                if (char.IsUpper(c))
-                {
-                    result[i] = char.ToLower(c);
-                }
-                else
-                {
-                    result[i] = c;
-                }
-            }
-            i++;
-        }
-        return new string(result);
-    }
 
-    static int[] CountPosSumNeg(float[] result)
+    public static void Main()
+
     {
-        if (result.Length == 0)
-            return new int[0];
-        int count = 0;
-        float sum = 0;
-        foreach(float f in result)
-        {
-            if (f > 0)
-                count++;
-            if (f < 0)
-                sum += f;
-        }
-        
-        return new int[] { count, ((int)sum) };
+
+        Client client = null;
+
+        client = new Client(new CocaColaFactory()); client.Run();
+
+        client = new Client(new PepsiFactory()); client.Run();
 
     }
 
-    static void CountPosSumNegWrite(int[] result)
-    {
-        if (result.Length == 0)
-            Console.WriteLine("[]");
-        else
-            Console.WriteLine("[" + result[0] + ", " + result[1] + "]");
+}
+class Client
+{
+    private AbstractWater water; private AbstractBottle bottle;
 
+    public Client(AbstractFactory factory)
+    {
+        // Абстрагирование процессов инстанцирования. water = factory.CreateWater();
+        bottle = factory.CreateBottle();
 
     }
+    public void Run()
 
-    static void Main()
     {
-        Console.WriteLine(InvertCase("Happy bursdfsdD"));
-        Console.WriteLine(InvertCase("мама"));
-        Console.WriteLine(InvertCase("ПРорывО"));
 
-        CountPosSumNegWrite(CountPosSumNeg([]));
-        CountPosSumNegWrite(CountPosSumNeg([1, 2, 5, -3, -5]));
+        // Абстрагирование вариантов использования. bottle.Interact(water);
 
+    }
+}
+abstract class AbstractFactory
+
+{
+
+    public abstract AbstractWater CreateWater(); public abstract AbstractBottle CreateBottle();
+
+}
+
+class CocaColaFactory : AbstractFactory
+{
+    public override AbstractWater CreateWater()
+    {
+        return new CocaColaWater();
+    }
+    public override AbstractBottle CreateBottle()
+    {
+        return new CocaColaBottle();
+    }
+}
+class PepsiFactory : AbstractFactory
+{
+    public override AbstractWater CreateWater()
+    {
+        return new PepsiWater();
+    }
+    public override AbstractBottle CreateBottle()
+    {
+            return new PepsiBottle();
+    }
+}
+abstract class AbstractWater { }
+class CocaColaWater : AbstractWater { }
+class PepsiWater : AbstractWater { }
+abstract class AbstractBottle
+{
+    public abstract void Interact(AbstractWater water);
+}
+class CocaColaBottle : AbstractBottle
+{
+    public override void Interact(AbstractWater water)
+
+    {
+        Console.WriteLine(this + " interacts with " + water);
+    }
+
+}
+class PepsiBottle : AbstractBottle
+{
+    public override void Interact(AbstractWater water)
+    {
+        Console.WriteLine(this + " interacts with " + water);
     }
 }
