e757bfa15c76c4bee4804b6120a6ae3028d23c7a
vinpap2008S <mirzoevsergej881@gmail.com>
2024-06-21 14:17:15 +0300

    патерн

--- c#/Program.cs
+++ c#/Program.cs
@@ -1,183 +1,113 @@
-﻿using System;
-using System.Diagnostics;
-using System.Security.Cryptography;
-using static ComputerShop;
-public class ComputerShop
-
+﻿
+class SubsystemA
 {
-    public void ConstructComputer(ComputerBuilder computerBuilder)
-    {
-        computerBuilder.BuildMotherboard();
-        computerBuilder.BuildProcessor();
-        computerBuilder.BuildHardDisk();
-        computerBuilder.BuildScreen();
-    }
-
-public abstract class ComputerBuilder
-
-    {
-        public Computer Computer { get; set; }
-
-        public abstract void BuildMotherboard();
-        public abstract void BuildProcessor();
-        public abstract void BuildHardDisk();
-        public abstract void BuildScreen();
-    }
+    public void A1()
+    { }
 }
-public class LaptopBuilder : ComputerBuilder
+class SubsystemB
 {
-    public LaptopBuilder()
-    {
-        Computer = new Computer(ComputerTyp.Laptop);
-    }
-    public override void BuildMotherboard()
-    {
-        Computer.MotherBoard = "DELL MotherBoard";
-    }
+    public void B1()
+    { }
+}
+class SubsystemC
+{
+    public void C1()
+    { }
+}
 
-    public override void BuildProcessor()
-    {
-        Computer.Processor = "Intel Core 2 Duo";
-    }
+public class Facade
+{
+    SubsystemA subsystemA;
+    SubsystemB subsystemB;
+    SubsystemC subsystemC;
 
-    public override void BuildHardDisk()
+    public Facade(SubsystemA sa, SubsystemB sb, SubsystemC sc)
     {
-        Computer.HardDisk = "250GB";
+        subsystemA = sa;
+        subsystemB = sb;
+        subsystemC = sc;
     }
-    public override void BuildScreen()
+    public void Operation1()
     {
-        Computer.Screen = "15.4-inch (1280 x 800)";
+        subsystemA.A1();
+        subsystemB.B1();
+        subsystemC.C1();
+    }
+    public void Operation2()
+    {
+        subsystemB.B1();
+        subsystemC.C1();
     }
 }
 
-public class DesktopBuilder : ComputerBuilder
+class Client
 {
-
-    public DesktopBuilder()
-
+    public void Main()
     {
-        Computer = new Computer(ComputerTyp.Desktop);
-
+        Facade facade = new Facade(new SubsystemA(), new SubsystemB(), new SubsystemC());
+        facade.Operation1();
+        facade.Operation2();
     }
-
-    public override void BuildMotherboard()
-
+}
+class Client
+{
+    public void Request(Target target)
     {
-        Computer.MotherBoard = "Asus P6X58D Premium";
+        target.Request();
     }
+}
+// класс, к которому надо адаптировать другой класс   
+class Target
+{
+    public virtual void Request()
+    { }
+}
 
-    public override void BuildProcessor()
+// Адаптер
+class Adapter : Target
+{
+    private Adaptee adaptee = new Adaptee();
 
+    public override void Request()
     {
-
-        Computer.Processor = "Intel Xeon 7500";
-    }
-    public override void BuildHardDisk()
-    {
-        Computer.HardDisk = "2TB";
-    }
-    public override void BuildScreen()
-    {
-        Computer.Screen = "21 inch (198@ x 1200)";
+        adaptee.SpecificRequest();
     }
 }
 
-public class AppleBuilder : ComputerBuilder
-
+// Адаптируемый класс
+class Adaptee
 {
-
-    public AppleBuilder()
-
+    public void SpecificRequest()
+    { }
+}
+class Client
+{
+    public void Request(Target target)
     {
-        Computer = new Computer(ComputerTyp.Apple);
-    }
-
-    public override void BuildMotherboard()
-
-    {
-        Computer.MotherBoard = "iMac G5 PowerPC";
-
-    }
-
-    public override void BuildProcessor()
-
-    {
-
-        Computer.Processor = "Intel Core 2 Duo";
-
-    }
-
-    public override void BuildHardDisk()
-
-    {
-        Computer.HardDisk = "32GB";
-    }
-    public override void BuildScreen()
-    {
-        Computer.Screen = "24 inch (198@ x 1200)";
+        target.Request();
     }
 }
-public class Computer
-
+// класс, к которому надо адаптировать другой класс   
+class Target
 {
+    public virtual void Request()
+    { }
+}
 
-    private ComputerTyp _computerTyp;
+// Адаптер
+class Adapter : Target
+{
+    private Adaptee adaptee = new Adaptee();
 
-    public string MotherBoard { get; set; }
-    public string Processor { get; set; }
-    public string HardDisk { get; set; }
-    public string Screen { get; set; }
-    public Computer(ComputerTyp computerTyp)
+    public override void Request()
     {
-        _computerTyp = computerTyp;
-    }
-
-    public void DisplayConfiguration()
-    {
-        string message;
-
-        message = string.Format("Computer: {0}", _computerTyp);
-        Console.WriteLine(message);
-
-        message = string.Format("Motherboard: {0}", MotherBoard);
-        Console.WriteLine(message);
-
-        message = string.Format("Processor: {0}", Processor);
-        Console.WriteLine(message);
-
-        message = string.Format("Harddisk: {0}", HardDisk);
-        Console.WriteLine(message);
-
-        message = string.Format("Screen: {0}", Screen);
-        Console.WriteLine(message);
-
-        Console.WriteLine();
+        adaptee.SpecificRequest();
     }
 }
-public enum ComputerTyp
+
+// Адаптируемый класс
+class Adaptee
 {
-    Laptop,
-    Desktop,
-    Apple
-}
-class Program
-{
-    private static void Main()
-    {
-        ComputerShop computerShop = new ComputerShop();
-        ComputerBuilder computerBuilder;
-
-        computerBuilder = new LaptopBuilder();
-        computerShop.ConstructComputer(computerBuilder);
-        computerBuilder.Computer.DisplayConfiguration();
-
-        computerBuilder = new DesktopBuilder();
-        computerShop.ConstructComputer(computerBuilder);
-        computerBuilder.Computer.DisplayConfiguration();
-
-        computerBuilder = new AppleBuilder();
-        computerShop.ConstructComputer(computerBuilder);
-        computerBuilder.Computer.DisplayConfiguration();
-        Console.ReadKey();
-    }
+    public void SpecificRequest()
+    { }
 }
